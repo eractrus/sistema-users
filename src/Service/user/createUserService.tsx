d23cb7d301eaffbd7a423ea13108d48ff1a2da6b
Eractrus de Judá <eractrus@gmail.com>
2023-12-18 20:38:05 -0300

    Senha dos usuários estão criptografadas por: Eractrus

--- src/Service/user/createUserService.tsx
+++ src/Service/user/createUserService.tsx
@@ -1,12 +1,19 @@
 import prismaClient from "../../Prisma"
+import { hash } from 'bcryptjs'
 import { PropsUser } from '../../types/user'
 
 export default class CreateUserService {
     async executeService({ apelido, email, senha }: PropsUser) {
 
+        const senhaHash = await hash(senha, 8)
+
         const service = await prismaClient.user.create({
             data: {
-                apelido: apelido, email:email, senha:senha
+                apelido: apelido, email: email, senha: senhaHash
+            },
+            select: {
+                apelido: true,
+                email: true,
             }
         })
 
